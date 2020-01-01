dcde669fe4e15bb1a8946dde41eaec950a4f07cb
Dan Regev <dan.regev@mail.huji.ac.il>
2020-01-01 20:00:52 +0200

    Finished Parser (hopefully...)

--- Parser.py
+++ Parser.py
@@ -2,70 +2,60 @@ import xml.etree.ElementTree as ET
 
 from Tokenizer import *
 
+EXPRESSION_LIST = 'expressionList'
+
+TERM = 'term'
+
+RETURN_STATEMENT = 'returnStatement'
+
+DO_STATEMENT = 'doStatement'
+
+WHILE_STATEMENT = 'whileStatement'
+
+IF_STATEMENT = 'ifStatement'
+
+EXPRESSION = 'expression'
+
+UNARY_OPS = '-~'
+
+KEYWORD_CONSTS = 'truefalsenullthis'
+
+PERIOD = '.'
+
+OPS = '+-*/&|<>='
+
 ELSE = 'else'
-
 EQUALS = '='
-
 CLOSE_BRACKETS = ']'
-
 OPEN_BRACKETS = '['
-
 LET_STATEMENT = 'letStatement'
-
 IF = 'if'
-
 RETURN = 'return'
-
 WHILE = 'while'
-
 LET = 'let'
-
 DO = 'do'
-
 STATEMENTS = 'statements'
-
 VAR_DEC = 'varDec'
-
 SUBROUTINE_BODY = 'subroutineBody'
-
 VAR = 'var'
-
 PARAMETER_LIST = 'parameterList'
-
 CLOSE_PAR = ')'
-
 OPEN_PAR = '('
-
 VOID = 'void'
-
 SUBROUTINE_DEC = 'subroutineDec'
-
 SEMICOLON = ';'
-
 COMMA = ','
-
 CLASS_VAR_DEC = 'classVarDec'
-
 CLASS_TAG = 'class'
-
 IDENTIFIER = 'identifier'
-
 SYMBOL = 'symbol'
-
 END_BLOCK = '}'
-
 CONSTRUCTOR = 'constructor'
-
 FUNCTION = 'function'
-
 METHOD = 'method'
-
 STATIC = 'static'
-
 FIELD = 'field'
-
 START_BLOCK = '{'
-
 CLASS_KEYWORD = 'class'
 
 
@@ -87,6 +77,7 @@ class Parser:
         """
         # TODO: implement
         new_element = ET.Element()
+        self.__compile_class(new_element)
         tree = ET.ElementTree(new_element)
         return tree
 
@@ -133,7 +124,7 @@ class Parser:
         self.__compile_type(new_element)
         self.__compile_identifier(new_element)
         next_token = self.__tokenizer.peek().get_content()
-        while next_token == COMMA:  # TODO Infinite loop?
+        while next_token == COMMA:
             self.__compile_symbol(new_element, next_token)
             self.__compile_identifier(new_element)
             next_token = self.__tokenizer.peek().get_content()
@@ -146,7 +137,8 @@ class Parser:
         elif next_token.get_type() == KEYWORD:
             self.__compile_keyword(element)
         else:
-            raise RuntimeError ("Type must be identifier or keyword but token" + next_token.get_content() +  "was of type " + next_token.get_type() )
+            raise RuntimeError(
+                "Type must be identifier or keyword but token" + next_token.get_content() + "was of type " + next_token.get_type())
 
     def __compile__subroutine_dec(self, element):
         new_element = ET.Element(SUBROUTINE_DEC)
@@ -171,12 +163,11 @@ class Parser:
         self.__compile_type(new_element)
         self.__compile_identifier(new_element)
         next_token = self.__tokenizer.peek().get_content()
-        while next_token is COMMA:  # TODO Infinite loop?
+        while next_token is COMMA:
             self.__compile_symbol(new_element, COMMA)
             self.__compile_type(new_element)
             self.__compile_identifier(new_element)
             next_token = self.__tokenizer.peek().get_content()
-
 
     def __compile_subroutine_body(self, element):
         new_element = ET.Element(SUBROUTINE_BODY)
@@ -231,11 +222,17 @@ class Parser:
         self.__compile_symbol(new_element, SEMICOLON)
 
     def __compile_expression(self, element):
-        new_element = ET.Element(LET_STATEMENT)
+        new_element = ET.Element(EXPRESSION)
         element.append(new_element)
+        self.__compile_term(new_element)
+        next_token = self.__tokenizer.peek().get_content()
+        while next_token in OPS:
+            self.__compile_symbol(new_element, next_token)
+            self.__compile_term(new_element)
+            next_token = self.__tokenizer.peek().get_content()
 
     def __compile_if(self, element):
-        new_element = ET.Element(LET_STATEMENT)
+        new_element = ET.Element(IF_STATEMENT)
         element.append(new_element)
         self.__compile_keyword(new_element)
         self.__compile_symbol(new_element, OPEN_PAR)
@@ -251,7 +248,7 @@ class Parser:
             self.__compile_symbol(new_element, END_BLOCK)
 
     def __compile_while(self, element):
-        new_element = ET.Element(LET_STATEMENT)
+        new_element = ET.Element(WHILE_STATEMENT)
         element.append(new_element)
         self.__compile_keyword(new_element)
         self.__compile_symbol(new_element, OPEN_PAR)
@@ -262,22 +259,78 @@ class Parser:
         self.__compile_symbol(new_element, END_BLOCK)
 
     def __compile_do(self, element):
-        new_element = ET.Element(LET_STATEMENT)
+        new_element = ET.Element(DO_STATEMENT)
         element.append(new_element)
         self.__compile_keyword(new_element)
         self.__compile_subroutine_call(new_element)
         self.__compile_symbol(new_element, SEMICOLON)
 
     def __compile_subroutine_call(self, element):
+        new_element = element
+        self.__compile_identifier(new_element)
+        next_token = self.__tokenizer.peek().get_content()
+        if next_token == OPEN_PAR:
+            self.__compile_symbol(new_element, OPEN_PAR)
+            self.__compile_expression_list(new_element)
+            self.__compile_symbol(new_element, CLOSE_PAR)
+        elif next_token == PERIOD:
+            self.__compile_symbol(new_element, PERIOD)
+            self.__compile_identifier(new_element)
+            self.__compile_symbol(new_element, OPEN_PAR)
+            self.__compile_expression_list(new_element)
+            self.__compile_symbol(new_element, CLOSE_PAR)
 
     def __compile_return(self, element):
-        new_element = ET.Element(LET_STATEMENT)
+        new_element = ET.Element(RETURN_STATEMENT)
         element.append(new_element)
+        self.__compile_keyword(new_element)
+        next_token = self.__tokenizer.peek().get_content()
+        if next_token != SEMICOLON:
+            self.__compile_expression(new_element)
+        self.__compile_symbol(new_element, SEMICOLON)
 
     def __compile_term(self, element):
-        new_element = ET.Element(LET_STATEMENT)
+        new_element = ET.Element(TERM)
         element.append(new_element)
+        next_token = self.__tokenizer.peek()
+        if next_token.get_type() == INTEGER_CONSTANT:
+            newer_element = ET.Element(INTEGER_CONSTANT)
+            newer_element.text = self.__tokenizer.next_token().get_content()
+            new_element.append(newer_element)
+        elif next_token.get_type() == STRING_CONSTANT:
+            newer_element = ET.Element(STRING_CONSTANT)
+            newer_element.text = self.__tokenizer.next_token().get_content()
+            new_element.append(newer_element)
+        elif next_token.get_content() in KEYWORD_CONSTS:
+            self.__compile_keyword(new_element)
+        elif next_token.get_type() == IDENTIFIER:
+            self.__compile_identifier(new_element)
+            next_token = self.__tokenizer.peek()
+            if next_token.get_content() == OPEN_BRACKETS:
+                self.__compile_symbol(new_element, OPEN_BRACKETS)
+                self.__compile_expression(new_element)
+                self.__compile_symbol(new_element, CLOSE_BRACKETS)
+            elif next_token.get_content() == OPEN_PAR:
+                self.__compile_symbol(new_element, OPEN_PAR)
+                self.__compile_subroutine_call(new_element)
+                self.__compile_symbol(new_element, CLOSE_PAR)
+        elif next_token.get_content() in UNARY_OPS:
+            self.__compile_symbol(new_element, next_token.get_content())
+            self.__compile_term(new_element)
 
     def __compile_expression_list(self, element):
-        new_element = ET.Element(LET_STATEMENT)
+        new_element = ET.Element(EXPRESSION_LIST)
         element.append(new_element)
+        next_token = self.__tokenizer.peek()
+        if self.__is_term(next_token):
+            self.__compile_expression(new_element)
+        else:
+            new_element.text = '\n'
+            return
+        next_token = self.__tokenizer.peek()
+        while next_token.get_content() == COMMA:
+            self.__compile_symbol(new_element, COMMA)
+            self.__compile_expression(new_element)
+
+    def __is_term(self, token: Token):
+        return token.get_type() == STRING_CONSTANT or token.get_type() == INTEGER_CONSTANT or token.get_content() in KEYWORD_CONSTS or token.get_type() == IDENTIFIER or token.get_content() in UNARY_OPS
